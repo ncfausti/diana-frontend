859d2231a675fa81705a683e6612479957b227a5
Nick Fausti <ncfausti@gmail.com>
2016-08-12 14:55:52 -0400

    fixed infosection css

--- src/js/components/Header/SubmissionInfoSection.js
+++ src/js/components/Header/SubmissionInfoSection.js
@@ -3,16 +3,9 @@ import React from 'react';
 export default class SubmissionInfoSection extends React.Component {
 	render() {
 		let bugID = 2010;
-		var sectionStyle = {
-			backgroundColor:'#fff',
-		}
-		var spanStyle = {
-			color:'#111',
-		}
 		return (
-			<div id="infoSection" style={sectionStyle}>
-			<h2>113 bugs accepted</h2><i class="material-icons md-36">chevron_right</i>
-			<span style={spanStyle}>Bug ID: {bugID}</span>
+			<div id="infoSection">
+			<h2>113 bugs accepted <span class="light">from 200 submissions</span></h2>
 			</div>
 		);
 	}
