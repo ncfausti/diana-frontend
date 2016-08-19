41b989592a2aba71beab70108868a85871a3ee03
Nick Fausti <ncfausti@gmail.com>
2016-08-18 22:40:27 -0400

    added Submissions page

---
+++ src/js/components/Submissions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import BugTable from './BugTable';
+
+
+export default class Submissions extends React.Component {
+	render() {
+		
+		return (
+			<div>submissions</div>
+		);
+	}
+}
